394d9b8dad53c597b547df9352ac5de8a0c0249b
Gaston Colaneri <colaneri_gaston@hotmail.com>
2021-10-06 01:18:06 -0300

    Cambios mínimos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let divPrueba = document.querySelector('#prueba');
+let listaJuegos = [];
 
 const pruebaAPI = async () => {
   let datos = await fetch(
@@ -10,3 +11,30 @@ const pruebaAPI = async () => {
   })} </ul>`;
   console.log(respuesta);
 };
+
+for (let i = 0; i < 2; i++) {
+  const gamesAPI = async () => {
+    let datos = await fetch(`https://www.cheapshark.com/api/1.0/games?id=${i}`);
+    let respuesta = await datos.json();
+    listaJuegos.push(respuesta.info.title);
+  };
+  gamesAPI();
+}
+
+// Materialize JS
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var options = {
+    data: {
+      Apple: null,
+      Microsoft: null,
+      Google: null,
+    },
+  };
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+});
