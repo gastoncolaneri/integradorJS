229b2d15ce6033645dc6bc529056c688595e34d3
Gaston Colaneri <colaneri_gaston@hotmail.com>
2021-10-09 10:33:45 -0300

    Cambios esteticos

--- js/main.js
+++ js/main.js
@@ -61,3 +61,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems);
+});
