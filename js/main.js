f64b570d490eaad77fd6bbfa80cd636749401c2f
Gaston Colaneri <colaneri_gaston@hotmail.com>
2021-10-27 23:29:44 -0300

    Cambios estéticos en el buscador de juegos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let divPrueba = document.querySelector('#prueba');
+let containerCardGames = document.querySelector('#containerCard');
+let loader = document.querySelector('#loader');
 let carouselDiv = document.querySelector('#carousel');
 let imgOne = document.querySelector('#imgOne');
 let imgTwo = document.querySelector('#imgTwo');
@@ -7,30 +8,40 @@ let imgFour = document.querySelector('#imgFour');
 let imgFive = document.querySelector('#imgFive');
 let listaElementos = [imgOne, imgTwo, imgThree, imgFour, imgFive];
 
-let listaJuegos = [];
-const CORSURL = 'https://cors-anywhere.herokuapp.com/';
+let listaJuegos = {};
 const APIURL = 'https://www.freetogame.com/api/games';
 const URLGAMEID = 'https://www.freetogame.com/api/game?id=';
 
-const pruebaAPI = async () => {
+const gamesListComplete = async () => {
   let datos = await fetch(APIURL);
   let respuesta = await datos.json();
+  let iconPlatform = '';
+  loader.classList.add('hiddenLoader');
 
-  divPrueba.innerHTML = `${respuesta.map((juego) => {
-    return `
-    <div class="col s6 m3">
-      <div class="card">
+  {
+    respuesta.map((juego) => {
+      {
+        juego.platform == 'PC (Windows)'
+          ? (iconPlatform = 'desktop_windows')
+          : (iconPlatform = 'web');
+      }
+      containerCardGames.innerHTML += `<div class="col s9 m5 l4 offset-s1">
+      <div class="card cardContent hoverable">
         <div class="card-image">
           <img src="${juego.thumbnail}">
           <span class="card-title">${juego.title}</span>
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
-        <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        <div class="card-content description">
+          <p class="truncate ">${juego.short_description}</p>
+          <div class="valign-wrapper mt15 justifyFE">
+          <div class="btn-small genreTag ">${juego.genre}</div>
+          <i class="material-icons">${iconPlatform}</i>
+          </div>
         </div>
       </div>
     </div>`;
-  })}</div>`;
+    });
+  }
 };
 
 const carouselAPI = async (i) => {
@@ -52,42 +63,52 @@ const carouselGames = () => {
   }
 };
 
+const prueba = (elemtn) => {
+  console.log(elemtn);
+};
+const gamesAutocomplete = async () => {
+  let datos = await fetch(APIURL);
+  let respuesta = await datos.json();
+  respuesta.map((juego) => {
+    listaJuegos[juego.title] = juego.thumbnail;
+  });
+};
+
 // Materialize JS
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
+  let elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems, {});
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var options = {
-    data: {
-      Apple: null,
-      Microsoft: null,
-      Google: null,
+  let options = {
+    data: listaJuegos,
+    onAutocomplete: function (texto) {
+      prueba(texto);
     },
   };
-  var elems = document.querySelectorAll('.autocomplete');
-  var instances = M.Autocomplete.init(elems, options);
+  let elems = document.querySelectorAll('.autocomplete');
+  let instances = M.Autocomplete.init(elems, options);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
+  let elems = document.querySelectorAll('select');
+  let instances = M.FormSelect.init(elems);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems);
+  let elems = document.querySelectorAll('.modal');
+  let instances = M.Modal.init(elems);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.tooltipped');
-  var instances = M.Tooltip.init(elems);
+  let elems = document.querySelectorAll('.tooltipped');
+  let instances = M.Tooltip.init(elems);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, {
+  let elems = document.querySelectorAll('.carousel');
+  let instances = M.Carousel.init(elems, {
     fullWidth: true,
     indicators: true,
     duration: 500,
@@ -95,18 +116,27 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems);
+  let elems = document.querySelectorAll('.parallax');
+  let instances = M.Parallax.init(elems);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems);
+  let elems = document.querySelectorAll('.fixed-action-btn');
+  let instances = M.FloatingActionButton.init(elems);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var textNeedCount = document.querySelectorAll(
+  let textNeedCount = document.querySelectorAll(
     '#input_text, #textarea1, #textarea2'
   );
   M.CharacterCounter.init(textNeedCount);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  let elems = document.querySelectorAll('.dropdown-trigger');
+  let options = {
+    coverTrigger: false,
+    constrainWidth: false,
+  };
+  let instances = M.Dropdown.init(elems, options);
 });
