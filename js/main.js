24ac1a550b6f327d9f3a700af88cc362b515ecaf
Gaston Colaneri <colaneri_gaston@hotmail.com>
2021-10-28 23:57:37 -0300

    Cambios de funcionalidad en la sección de búsqueda

--- js/main.js
+++ js/main.js
@@ -6,25 +6,33 @@ let imgTwo = document.querySelector('#imgTwo');
 let imgThree = document.querySelector('#imgThree');
 let imgFour = document.querySelector('#imgFour');
 let imgFive = document.querySelector('#imgFive');
+let showBtn = document.querySelector('#showBtn');
 let listaElementos = [imgOne, imgTwo, imgThree, imgFour, imgFive];
-
 let listaJuegos = {};
+let games, gamesFilter;
+let genre = 'allGenre';
+let platform = 'allPlatforms';
+let orderBy = 'relevance';
+
 const APIURL = 'https://www.freetogame.com/api/games';
 const URLGAMEID = 'https://www.freetogame.com/api/game?id=';
 
-const gamesListComplete = async () => {
+const APIrequest = async () => {
   let datos = await fetch(APIURL);
   let respuesta = await datos.json();
-  let iconPlatform = '';
-  loader.classList.add('hiddenLoader');
+  games = respuesta;
+  mapCard(games);
+};
 
-  {
-    respuesta.map((juego) => {
-      {
-        juego.platform == 'PC (Windows)'
-          ? (iconPlatform = 'desktop_windows')
-          : (iconPlatform = 'web');
-      }
+const mapCard = (data) => {
+  containerCardGames.innerHTML = '';
+  try {
+    data.map((juego) => {
+      juego.platform == 'PC (Windows)'
+        ? (iconPlatform = 'desktop_windows')
+        : (iconPlatform = 'web');
+      listaJuegos[juego.title] = juego.thumbnail;
+      loader.classList.add('hiddenLoader');
       containerCardGames.innerHTML += `<div class="col s9 m5 l4 offset-s1">
       <div class="card cardContent hoverable">
         <div class="card-image">
@@ -41,7 +49,20 @@ const gamesListComplete = async () => {
       </div>
     </div>`;
     });
+  } catch (error) {
+    loader.classList.add('hiddenLoader');
+    Swal.fire({
+      title: 'No results found',
+      icon: 'error',
+      confirmButtonText: 'Ok',
+    });
   }
+};
+
+const gamesListComplete = () => {
+  loader.classList.remove('hiddenLoader');
+  mapCard(games);
+  showBtn.classList.add('disabled');
 };
 
 const carouselAPI = async (i) => {
@@ -63,8 +84,33 @@ const carouselGames = () => {
   }
 };
 
-const prueba = (elemtn) => {
-  console.log(elemtn);
+const filterAutocomplete = (elemtn) => {
+  gamesFilter = games.filter(
+    (game) => game.title.toLowerCase() === elemtn.toLowerCase()
+  );
+  if ((gamesFilter.length = 1)) {
+    console.log(gamesFilter);
+    gamesFilter[0].platform == 'PC (Windows)'
+      ? (iconPlatform = 'desktop_windows')
+      : (iconPlatform = 'web');
+
+    containerCardGames.innerHTML = `<div class="col s11 m6 l6 offset-s1">
+      <div class="card cardContent hoverable">
+        <div class="card-image">
+          <img src="${gamesFilter[0].thumbnail}">
+          <span class="card-title">${gamesFilter[0].title}</span>
+        </div>
+        <div class="card-content description">
+          <p class="truncate ">${gamesFilter[0].short_description}</p>
+          <div class="valign-wrapper mt15 justifyFE">
+          <div class="btn-small genreTag ">${gamesFilter[0].genre}</div>
+          <i class="material-icons">${iconPlatform}</i>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    showBtn.classList.remove('disabled');
+  }
 };
 const gamesAutocomplete = async () => {
   let datos = await fetch(APIURL);
@@ -72,6 +118,52 @@ const gamesAutocomplete = async () => {
   respuesta.map((juego) => {
     listaJuegos[juego.title] = juego.thumbnail;
   });
+};
+
+const selectedGenre = (event) => {
+  genre = event.target.value;
+  showGamesFiltered();
+};
+
+const selectedPlatform = (event) => {
+  platform = event.target.value;
+  showGamesFiltered();
+};
+
+const sortBy = (event) => {
+  orderBy = event.target.value;
+  showGamesFiltered();
+};
+
+const showGamesFiltered = async () => {
+  containerCardGames.innerHTML = '';
+  loader.classList.remove('hiddenLoader');
+  if (
+    genre !== undefined &&
+    genre !== 'allGenre' &&
+    platform !== undefined &&
+    platform !== 'allPlatforms'
+  ) {
+    let datos = await fetch(
+      APIURL + `?platform=${platform}&category=${genre}&sort-by=${orderBy}`
+    );
+    let respuesta = await datos.json();
+    mapCard(respuesta);
+  } else if (genre !== undefined && genre !== 'allGenre') {
+    let datos = await fetch(APIURL + `?category=${genre}&sort-by=${orderBy}`);
+    let respuesta = await datos.json();
+    mapCard(respuesta);
+  } else if (platform !== undefined && platform !== 'allPlatforms') {
+    let datos = await fetch(
+      APIURL + `?platform=${platform}&sort-by=${orderBy}`
+    );
+    let respuesta = await datos.json();
+    mapCard(respuesta);
+  } else if (platform === 'allPlatforms' && genre === 'allGenre') {
+    let datos = await fetch(APIURL + `?sort-by=${orderBy}`);
+    let respuesta = await datos.json();
+    mapCard(respuesta);
+  }
 };
 
 // Materialize JS
@@ -84,7 +176,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let options = {
     data: listaJuegos,
     onAutocomplete: function (texto) {
-      prueba(texto);
+      filterAutocomplete(texto);
     },
   };
   let elems = document.querySelectorAll('.autocomplete');
